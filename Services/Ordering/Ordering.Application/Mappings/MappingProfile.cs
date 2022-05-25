384cff12811da5d22cd53da61ef996023f6af3ea
evren erin <evren.erin@gmail.com>
2022-05-25 23:37:32 +0300

    Order extended

--- Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,9 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +12,13 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Mappings
 {
-    public class MappingProfile: Profile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
     }
 }
